f4320930602a0e9aee554e0de5390b140e8196c4
Cristina Folgueral Gutiérrez <cris@obturecode.com>
2015-03-02 19:51:32 +0100

    trabajando en listado

--- VallaHackathon/app/src/main/java/com/obturecode/vallahackathon/view/ListActivity.java
+++ VallaHackathon/app/src/main/java/com/obturecode/vallahackathon/view/ListActivity.java
@@ -1,9 +1,11 @@
-package com.obturecode.vallahackathon;
+package com.obturecode.vallahackathon.view;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.obturecode.vallahackathon.R;
 
 
 public class ListActivity extends ActionBarActivity {
@@ -12,28 +14,5 @@ public class ListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
